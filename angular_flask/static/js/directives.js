1f0d5a166727fedf227627aca9974168c67b36f5
vanshaj2608 <vanshajdixit@gmail.com>
2017-11-05 05:53:40 +0530

    Update directives.js

--- angular_flask/static/js/directives.js
+++ angular_flask/static/js/directives.js
@@ -1,4 +1,27 @@
 'use strict';
 
-/* Directives */
 
+.directive('fileReader', function($rootScope) {
+  return {
+    scope: {
+      fileReader:"="
+    },
+    link: function(scope, element ) {
+      $(element).on('change', function(changeEvent  ) {
+        var files = changeEvent.target.files;
+        if (files.length) {
+          var r = new FileReader();
+          r.onload = function(e ) {
+              var contents = e.target.result;
+              scope.$apply(function () {
+                scope.fileReader = contents;
+                $rootScope.findBulkQuote(contents);
+              });
+          };
+          
+          r.readAsText(files[0]);
+        }
+      });
+    }
+  };
+});
